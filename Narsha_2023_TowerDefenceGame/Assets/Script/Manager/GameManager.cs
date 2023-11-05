36f08f65a84a6b10fe9ddbeda1182d676e87f38a
treemeew213 <112149394+treemeew213@users.noreply.github.com>
2023-11-05 21:38:40 +0900

    ddasd
    adasd

--- Narsha_2023_TowerDefenceGame/Assets/Script/Manager/GameManager.cs
+++ Narsha_2023_TowerDefenceGame/Assets/Script/Manager/GameManager.cs
@@ -4,35 +4,39 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace park
 {
-    public int towerHp;
-
-    public TextMeshProUGUI towerHpText;
-
-    public GameObject Lose;
-
-    public static GameManager instance;
-
-    private void Awake()
+    public class GameManager : MonoBehaviour
     {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(this);
-        }
-    }
+        public int towerHp;
 
-    private void Update()
-    {
-        towerHpText.text = "타워 체력 : " + towerHp;
-        
-        if (towerHp <= 0) {
-            Lose.SetActive(true);
-            Time.timeScale = 0f;
+        public TextMeshProUGUI towerHpText;
+
+        public GameObject Lose;
+
+        public static GameManager instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+
+        private void Update()
+        {
+            towerHpText.text = "타워 체력 : " + towerHp;
+
+            if (towerHp <= 0)
+            {
+                Lose.SetActive(true);
+                Time.timeScale = 0f;
+            }
         }
     }
 }
